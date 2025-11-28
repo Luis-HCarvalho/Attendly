b7baddef676a1e7fddffcb02572fdaf07ace6bb9
Lucas Hernandez <104871751+lucashernandez90@users.noreply.github.com>
2025-11-28 20:42:44 -0300

    Delete Attendly-main directory

--- Attendly-main/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.attendly.app',
-  appName: 'Attendly',
-  webDir: 'www'
-};
-
-export default config;
--- Attendly-main/src/app/model/IUser.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-  uid?: string;
-  name: string;
-  email: string;
-  role?: string;
-  photoURL?: string;
-  createdAt?: Date | string;
-}
